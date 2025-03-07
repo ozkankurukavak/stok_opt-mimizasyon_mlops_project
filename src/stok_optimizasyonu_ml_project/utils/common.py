78a38c4dd12667159dd9ab2af9aa354f48720a54
özkan kurukavak <158309837+ozkankurukavak@users.noreply.github.com>
2025-03-07 20:31:52 +0000

    common.py kodları düzenlendi

--- src/stok_optimizasyonu_ml_project/utils/common.py
+++ src/stok_optimizasyonu_ml_project/utils/common.py
@@ -11,42 +11,33 @@ import base64
 import pickle
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
-from typing import List
 
 
-@ensure_annotations # Bu fonksiyon, bir YAML dosyasını okuyup, içeriğini bir Box nesnesi olarak döndürür.
+@ensure_annotations
 def read_yaml(path_to_yaml: Path) -> Box:
     try:
         with open(path_to_yaml) as yaml_file:
-            content = yaml.safe_load(yaml_file)  # YAML içeriğini okur python sözlük tipine çevirir
-
-            if content is None:  # Eğer YAML dosyası boşsa
-                raise ValueError("YAML file is empty") # boşsa hata fırlatır
-
-            return Box(content)  # Box nesnesi olarak döndür yani keylere direk data.sütun olarak ulaşabiliriz
+            content = yaml.safe_load(yaml_file)  # YAML dosyasını okur.
+            return Box(content)  # YAML içeriğini Box formatında döndürür.
+    except BoxValueError:
+        raise ValueError("yaml file is empty")  # Eğer YAML dosyası boşsa hata fırlatır.
     except Exception as e:
-        raise e  # Diğer hataları yakala ve fırlat
-        
+        raise e  # Diğer hataları yakalar ve fırlatır.
     
     
-@ensure_annotations # path_to_directories listesindeki her yolu alır ve klasör oluşturur.
-def create_directories(path_to_directories: List[str], verbose=True):
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
     for path in path_to_directories:
-        os.makedirs(path, exist_ok=True)
-        if verbose:
-            print(f"Directory created: {path}")  # Yeni dizin oluşturulduğunda mesaj yazdır.
+        os.makedirs(path, exist_ok=True)  # Belirtilen dizinleri oluşturur, varsa var olanları geçer.
+        
 
-
-
-@ensure_annotations # Bu fonksiyon, bir dosyanın boyutunu KB (kilobayt) cinsinden döndürür.
+@ensure_annotations
 def get_size(path: Path) -> str:
     size_in_kb = round(os.path.getsize(path) / 1024)  # Dosyanın boyutunu KB cinsinden alır.
     return f"~ {size_in_kb} KB"  # Dosyanın boyutunu KB cinsinden döndürür.
 
-
-
-@ensure_annotations # Bu fonksiyon, Python objelerini (dictionary, liste, model vb.) pickle formatında kaydeder.
-def save_object(file_path: Path, obj): # file_path: Path → Kaydedilecek dosyanın yolu.obj → Kaydedilecek Python nesnesi (örneğin bir sözlük, model, veri çerçevesi vb.).
+@ensure_annotations
+def save_object(file_path: Path, obj):
     try:
         dir_path = os.path.dirname(file_path)  # Dosya yolunu alır.
         os.makedirs(dir_path, exist_ok=True)  # Dosya yolu mevcut değilse oluşturur.
@@ -54,7 +45,6 @@ def save_object(file_path: Path, obj): # file_path: Path → Kaydedilecek dosyan
             pickle.dump(obj, file_obj)  # Objeyi pickle ile kaydeder.
     except Exception as e:
         raise e  # Hata durumunda hatayı yakalar ve fırlatır.
-    
         
     
 def evaluate_model(X_train, y_train, X_test, y_test, models, param):
@@ -87,19 +77,19 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     
     
 @ensure_annotations
-def save_transformed_data(data: Any, file_path: Path, format: str = "csv"): # belirtilen veriyi (data parametresi) ve dosya yolunu (file_path parametresi) alarak, veriyi belirtilen formatta (CSV ya da JSON) kaydetmek için kullanılır.
+def save_transformed_data(data: Any, file_path: Path):
+    """
+    Veriyi belirtilen dosya yoluna kaydeder.
+    
+    :param data: Dönüştürülmüş veri (DataFrame)
+    :param file_path: Verinin kaydedileceği dosya yolu
+    """
     try:
-        dir_path = os.path.dirname(file_path)  # Dosya yolunun dizin kısmını alır.
-        os.makedirs(dir_path, exist_ok=True)  # Eğer dosyanın bulunduğu dizin mevcut değilse, dizini oluşturur.
-
-        if format == "csv":  # Eğer format "csv" olarak verilmişse
-            data.to_csv(file_path, index=False)  # Veriyi CSV formatında belirtilen dosyaya kaydeder (index'i kaydetmeden).
-        elif format == "json":  # Eğer format "json" olarak verilmişse
-            data.to_json(file_path, orient="records", indent=4)  # Veriyi JSON formatında kaydeder, her kayıt yeni bir satıra yazılır.
-        else:
-            raise ValueError(f"Geçersiz format: {format}")  # Eğer format ne CSV ne de JSON ise, hata fırlatır.
-
-        print(f"Veri başarıyla kaydedildi: {file_path}")  # Verinin başarıyla kaydedildiğini belirten mesajı yazdırır.
-
-    except Exception as e:  # Eğer bir hata oluşursa
-        raise RuntimeError(f"Veri kaydedilirken hata oluştu: {e}")  # Hata mesajıyla birlikte RuntimeError fırlatır.
+        dir_path = os.path.dirname(file_path)  # Dosya yolunu alır.
+        os.makedirs(dir_path, exist_ok=True)  # Dosya yolu mevcut değilse oluşturur.
+        
+        data.to_csv(file_path, index=False)  # Veriyi CSV formatında kaydeder.
+        print(f"Veri başarıyla kaydedildi: {file_path}")  # Kaydedildiğine dair mesaj yazdırır.
+        
+    except Exception as e:
+        raise e  # Hata durumunda hatayı yakalar ve fırlatır.
