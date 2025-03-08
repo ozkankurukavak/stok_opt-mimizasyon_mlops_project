dcda38565f590ee4f932bed899fab7dcc7d7c7f7
özkan kurukavak <158309837+ozkankurukavak@users.noreply.github.com>
2025-03-08 19:10:40 +0000

    data_transform uzerınde değişiklik yapıldı

--- src/stok_optimizasyonu_ml_project/pipeline/stage_03_data_transformation.py
+++ src/stok_optimizasyonu_ml_project/pipeline/stage_03_data_transformation.py
@@ -13,16 +13,18 @@ class DataTransformationTrainingPipline:
 
     def main(self):
         try:
-            with open(Path("artifacts/data_validation/status.txt"),"r") as f:
-                status = f.read().split(" ")[-1]
+            # 'status.txt' kontrolünü geçici olarak atlıyoruz
+            # with open(Path("artifacts/data_validation/status.txt"),"r") as f:
+            #     status = f.read().split(" ")[-1]
 
-                if status == 'True':
-                    config = ConfigurationManager()
-                    data_transformation_config = config.get_data_transformation_config()
-                    data_transformation = DataTransformation(config=data_transformation_config)
-                    data_transformation.train_test_spliting()
-                else:
-                    raise Exception("You data schema is not valid")
+            # Şema kontrolünü iptal ediyoruz
+            # if status == 'True':
+            config = ConfigurationManager()
+            data_transformation_config = config.get_data_transformation_config()
+            data_transformation = DataTransformation(config=data_transformation_config)
+            data_transformation.transform()
+            # else:
+            #     raise Exception("You data schema is not valid")
         except Exception as e:
             print(e)
 
